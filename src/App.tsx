400891aab9ef9924624da30ce259d10aecf4f7f2
maty0505git <maty0505@gmail.com>
2023-10-17 10:36:04 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./styles.css";
+import axios from "axios";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const okClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+  return (
+    <div className="App">
+      <Text color="red" fontSize="18px" />
+      <button onClick={okClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
+}
